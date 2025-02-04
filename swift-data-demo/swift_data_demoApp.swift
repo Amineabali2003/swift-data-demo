1452e50d36a8160b2f6609ae556dbd11c83030aa
ABALI Mohammed Amine <abalimohammedamine@gmail.com>
2025-02-04 18:59:09 +0100

    Initial Commit

---
+++ swift-data-demo/swift_data_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_data_demoApp.swift
+//  swift-data-demo
+//
+//  Created by ABALi Mohammed Amine on 04/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct swift_data_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift-data-demoTests/swift_data_demoTests.swift
@@ -0,0 +1,17 @@
+//
+//  swift_data_demoTests.swift
+//  swift-data-demoTests
+//
+//  Created by ABALi Mohammed Amine on 04/02/2025.
+//
+
+import Testing
+@testable import swift_data_demo
+
+struct swift_data_demoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
